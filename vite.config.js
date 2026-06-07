b9953ac3c6f51014e7d29b70deb55ed168f5036f
lightgi086-jpg <lightgi086@gmail.com>
2026-06-07 09:18:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: mode === 'admin' ? 'dist-admin' : 'dist',
-  },
-  esbuild: {
-    jsxInject: `import React from 'react'`
-  }
-}))
+})
